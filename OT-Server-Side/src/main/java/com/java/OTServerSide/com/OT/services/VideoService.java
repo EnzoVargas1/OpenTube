b60e693531c0a61a52c555c94347ee6982d2e350
Enzo Vargas <enzovargazzz65@gmail.com>
2022-04-22 21:14:40 -0500

    working on services

--- OT-Server-Side/src/main/java/com/java/OTServerSide/com/OT/services/VideoService.java
+++ OT-Server-Side/src/main/java/com/java/OTServerSide/com/OT/services/VideoService.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.java.OTServerSide.com.OT.exceptions.NoResourceFoundException;
+import com.java.OTServerSide.com.OT.models.Video;
+
 public interface VideoService {
 	public String addVideo(String name, boolean isPrivate, MultipartFile file)throws IOException;
-	
+	public Video getVideo(int id)throws IOException, IllegalStateException, NoResourceFoundException;
+	public Video updateVideo(int id, Video video)throws NoResourceFoundException;
 }
--- OT-Server-Side/src/main/java/com/java/OTServerSide/com/OT/services/VideoServiceImpl.java
+++ OT-Server-Side/src/main/java/com/java/OTServerSide/com/OT/services/VideoServiceImpl.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.java.OTServerSide.com.OT.exceptions.NoResourceFoundException;
 import com.java.OTServerSide.com.OT.models.Video;
 import com.java.OTServerSide.com.OT.repositiory.VideoRepository;
 
@@ -18,7 +19,30 @@ public class VideoServiceImpl implements VideoService {
 	public String addVideo(String name, boolean isPrivate, MultipartFile file) throws IOException {
 		// TODO Auto-generated method stub
 		Video vid = new Video(name, isPrivate, file.getInputStream());
+		videoRepository.save(vid);
 		return vid.getName();
 	}
 
+	@Override
+	public Video getVideo(int id) throws IOException, IllegalStateException, NoResourceFoundException{
+		// TODO Auto-generated method stub
+		Video vid = videoRepository.findById(id).orElse(null);
+		if(vid == null)
+			throw new NoResourceFoundException("No Resource Found");
+		return vid;
+	}
+
+	@Override
+	public Video updateVideo(int id,Video video) throws NoResourceFoundException {
+		// TODO Auto-generated method stub
+		Video vid = videoRepository.findById(id).orElse(null);
+		if(vid == null)
+			throw new NoResourceFoundException("No Resource Found");
+		vid = video;
+		videoRepository.save(vid);
+		return null;
+	}
+
+	
+	
 }
