1f7c9c2136efcfe2adf1925c9d8f4883e05091d9
Enzo Vargas <enzovargazzz65@gmail.com>
2022-06-19 23:16:45 -0500

    added a searchbar

--- OT-Client-Side/src/app/app.module.ts
+++ OT-Client-Side/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
@@ -14,6 +15,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent},
     ])
--- OT-Client-Side/src/app/home/home.component.ts
+++ OT-Client-Side/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  public searchInput:string = '';
+  public vids: Array<string> = ['Gasoline', 'SAD!', 'Java 101', 'bet', 
+  'stuff', 'Before I Forget', 'maladaptive daydreaming', 'Python 101', 'enzo'];
+  public results: Array<string> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fetchSeries(value: any):void{
+    if(value.length === 1){
+      this.results = this.vids.filter((name) => name.substring(0, this.searchInput.length).toLowerCase().trim()
+       == this.searchInput.toLowerCase().trim());
+      console.log(this.searchInput);
+      console.log(this.results);
+     }
+     else if(value.toLowerCase() === 'backspace'){
+      this.searchInput = this.searchInput.substring(0, this.searchInput.length-1);
+     }
   }
 
 }
